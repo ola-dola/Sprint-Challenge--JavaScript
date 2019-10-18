53e0ec404e5a9086f8d4dec4a936bef419eeee46
oredola <oredolaolamide@gmail.com>
2019-10-18 08:42:26 +0100

    Object tests passed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -35,20 +35,23 @@ const veloc = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyran.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(veloc.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyran.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyran.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!";
+}
+console.log(tyran.roar());
 
 
 // ==== Arrays ====
